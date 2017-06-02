7ad5672bcf14095c4c672ef62b38fb7c89851518
AndreKuzubov <smoll9521@gmail.com>
2017-06-02 15:54:13 +0300

    write params to log.txt

--- m_multiout_learning_multi_answer10/my_multiout_regression_multianswer.py
+++ m_multiout_learning_multi_answer10/my_multiout_regression_multianswer.py
@@ -11,22 +11,24 @@ import my_model_multi_out_multianswer as mModel
 
 folder = './testMultiAnswer'
 folderPostFix = 0
-while (os.path.exists(folder + str(folderPostFix)+'/')):
+while (os.path.exists(folder + str(folderPostFix) + '/')):
     folderPostFix = folderPostFix + 1
-folder = folder + str(folderPostFix)+'/'
+folder = folder + str(folderPostFix) + '/'
 
 if not os.path.exists(folder):
     os.makedirs(folder)
 
-print 'results in '+folder
+print 'results in ' + folder
 
 print 'load dataset.....'
 
 # dataset = mModel.load_datasets(n=100000, pointsInFrame=20)
 datasetSize = 700
 dataSetStep = 0.066
-dataset = mModel.load_dataset_uniform(n=datasetSize, step=dataSetStep, maxPics=3, scaleByFrame=0.8,
-                                      boxBorders=[0.1, 0.9])
+boxBorders = [0.1, 0.9]
+maxPics = 3
+dataset = mModel.load_dataset_uniform(n=datasetSize, step=dataSetStep, maxPics=maxPics, scaleByFrame=0.8,
+                                      boxBorders=boxBorders)
 
 # Split the data into training/testing sets
 dataset_X_train = dataset[0][:-25]
@@ -45,7 +47,7 @@ mrg = MultiOutputRegressor(GradientBoostingRegressor())
 
 startTime = datetime.datetime.now()
 
-learning_cicles = 1000
+learning_cicles = 100
 for i in xrange(learning_cicles):
     dataset_X_train, dataset_y_train = shuffle(dataset_X_train, dataset_y_train)
     print 'training ' + str(i) + ' left: ' + str(learning_cicles - i) + ' ..... '
@@ -71,6 +73,8 @@ f1 = open(folder + 'log.txt', 'w+')
 f1.write(str(mrg) + '\n' + '\n')
 f1.write('datasetSize= ' + str(datasetSize) + '\n')
 f1.write('dataSetStep= ' + str(dataSetStep) + '\n')
+f1.write('boxBorders= ' + str(boxBorders) + '\n')
+f1.write('maxPics= ' + str(maxPics) + '\n')
 f1.write('learning_cicles= ' + str(learning_cicles) + '\n\n')
 f1.write('spended time: ' + str(elapsedTime) + '\n\n')
 
@@ -93,4 +97,4 @@ for i in xrange(len(dataset_X_test)):
     mModel.draw_training_model(x, y, fileNameToSave=folder + 'training_model_' + str(i), show=0)
 
 print 'finish.....'
-print 'results in '+folder
+print 'results in ' + folder

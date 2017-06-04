cfe5772f6e74a4a50fcfc9f559d861cdf392248f
AndreKuzubov <smoll9521@gmail.com>
2017-06-04 14:23:26 +0300

    запись ошибок в errorLog.txt, рисование графика ошибки

--- m_multiout_learning_multi_answer10/my_multiout_regression_multianswer.py
+++ m_multiout_learning_multi_answer10/my_multiout_regression_multianswer.py
@@ -8,6 +8,7 @@ from sklearn.ensemble import GradientBoostingRegressor, BaggingRegressor, AdaBoo
 from sklearn.utils import shuffle
 from numpy import array
 import my_model_multi_out_multianswer as mModel
+import my_log_errors as errorLog
 
 folder = './testMultiAnswer'
 folderPostFix = 0
@@ -23,10 +24,10 @@ print 'results in ' + folder
 print 'load dataset.....'
 
 # dataset = mModel.load_datasets(n=100000, pointsInFrame=20)
-datasetSize = 700
-dataSetStep = 0.066
+datasetSize = 100
+dataSetStep = 0.197
 boxBorders = [0.1, 0.9]
-maxPics = 3
+maxPics = 2
 dataset = mModel.load_dataset_uniform(n=datasetSize, step=dataSetStep, maxPics=maxPics, scaleByFrame=0.8,
                                       boxBorders=boxBorders)
 
@@ -43,7 +44,7 @@ dataset_y_test = dataset[1][-25:]
 #     mrg = cPickle.load(fid)
 
 
-mrg = MultiOutputRegressor(GradientBoostingRegressor())
+mrg = MultiOutputRegressor(GradientBoostingRegressor(max_depth=10, learning_rate=0.03))
 
 startTime = datetime.datetime.now()
 
@@ -52,6 +53,9 @@ for i in xrange(learning_cicles):
     dataset_X_train, dataset_y_train = shuffle(dataset_X_train, dataset_y_train)
     print 'training ' + str(i) + ' left: ' + str(learning_cicles - i) + ' ..... '
     mrg.fit(dataset_X_train, dataset_y_train)
+
+    # count errors
+    errorLog.addErrorLog(mrg.predict(dataset_X_test) - array(dataset_y_test), (i+1) * datasetSize)
 
 endTime = datetime.datetime.now()
 elapsedTime = endTime - startTime
@@ -69,6 +73,9 @@ predictYs = list(predictYs)
 print str(predictYs)
 
 print 'print to log file.....'
+errorLog.saveLog(folder)
+errorLog.draw(folder + "error_function_", show=0)
+
 f1 = open(folder + 'log.txt', 'w+')
 f1.write(str(mrg) + '\n' + '\n')
 f1.write('datasetSize= ' + str(datasetSize) + '\n')
